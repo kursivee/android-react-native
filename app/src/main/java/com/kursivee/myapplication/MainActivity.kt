907d155b44424105d403a7ae355dcca99476ce44
kursivee <kursivee.dev@gmail.com>
2019-07-15 18:01:25 -0400

    Initial commit

---
+++ app/src/main/java/com/kursivee/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kursivee.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.fabric.ReactNativeConfig
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
